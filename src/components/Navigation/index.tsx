1318ad747968af78b1b7d244b1e9a02dbe26eeb4
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-06-24 13:57:07 -0600

    Menu

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -3,38 +3,41 @@ import { useState, type FC } from "react";
 import { Hamburger } from "./components/Hamburger";
 
 export const Navigation: FC = () => {
-    // to update values for all components, keep the state here
-    const [isOpen, setIsOpen] = useState(false);
+  // to update values for all components, keep the state here
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <S.NavigationStyled>
-            <S.NavigationListWrapper $isOpen={isOpen}>
-                <S.NavigationList>
-                    <li>
-                        <a href="/">Cybernetic</a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://codexcode.store/themes/cybernetic-website-template"
-                            target="_blank"
-                        >
-                            Pro version
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/infinite">Infinite</a>
-                    </li>
-                    <li>
-                        <a href="/contact">Contact</a>
-                    </li>
-                </S.NavigationList>
-            </S.NavigationListWrapper>
-            <Hamburger
-                state={{
-                    open: isOpen,
-                    setOpen: setIsOpen,
-                }}
-            />
-        </S.NavigationStyled>
-    );
+  return (
+    <S.NavigationStyled>
+      <S.NavigationListWrapper $isOpen={isOpen}>
+        <S.NavigationList>
+          <li>
+            <a href="/">Servicios</a>
+          </li>
+          <li>
+            <a
+              href="https://codexcode.store/themes/cybernetic-website-template"
+              target="_blank"
+            >
+              Contrataciones
+            </a>
+          </li>
+          <li>
+            <a href="/Nosotros">Nosotros</a>
+          </li>
+          <li>
+            <a href="/media">Media</a>
+          </li>
+          <li>
+            <a href="/contact">Contacto</a>
+          </li>
+        </S.NavigationList>
+      </S.NavigationListWrapper>
+      <Hamburger
+        state={{
+          open: isOpen,
+          setOpen: setIsOpen,
+        }}
+      />
+    </S.NavigationStyled>
+  );
 };
